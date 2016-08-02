90c3d2d8aaf47ac660b338f58d203c71809b5ab3
Tom Bartolucci <tbartolucci@gmail.com>
2016-08-02 16:32:08 -0400

    Finished basic data types.

---
+++ basic-data-types/run.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int a;
+long b, c;
+char e;
+float f;
+double g;
+
+scanf("%d %ld %ld %c %f %lf", &a,&b,&c,&e,&f,&g);
+printf("%d\n%ld\n%ld\n%c\n%f\n%lf\n", a,b,c,e,f,g);
+    // Complete the code.
+    return 0;
+}
